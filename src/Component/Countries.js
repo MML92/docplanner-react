fc6be48787b374939470548ff5814323c51372e8
FKHG <47415121+FKHG@users.noreply.github.com>
2020-03-01 18:40:40 +0100

    Add files via upload

---
+++ src/Component/Countries.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Country = props => {
+  return (
+    <div className="pays">
+      {props.arry.map(el => (
+        <div class="pay">
+          <img className="image" src={el.img} alt="image" />
+          <div class="more">
+            {" "}
+            <h3>{el.title}</h3>
+            <button>{el.button}</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Country;
